c07ba5e57d67ac4a1fa5499380a32fff20d162a6
Chen Xu <windoze@0d0a.com>
2020-01-30 21:49:50 +0800

    Swift 5

--- Package.swift
+++ Package.swift
@@ -1,5 +1,18 @@
+// swift-tools-version:5.1
+
 import PackageDescription
 
 let package = Package(
-    name: "Context"
+    name: "Context",
+    products: [
+        .library(
+            name: "Context",
+            targets: ["Context"])],
+    targets: [
+        .target(
+            name: "Context",
+            path: "Sources"),
+        .testTarget(
+            name: "ContextTests",
+            dependencies: ["Context"])]
 )
